ee8f4ccae1a410cbe5564736dbc0e44bafe64a39
Rozdandy <rozdandy@gmail.com>
2021-05-19 11:42:59 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51InnIvFl4lG8CSXrigliIUwiucMHa84VL56Mb6hdz0i1kfzB1caCiAB1Zf6gz2HeeI92cE6i8wKc3034MKlfGTTC00V0iYs0mq',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
