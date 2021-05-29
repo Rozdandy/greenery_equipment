7a5449d6cc0c12d206e2d09d11381d7d044e3e8c
Rozdandy <rozdandy@gmail.com>
2021-05-29 11:41:48 +0000

    Remove secret key and set DEBUG

--- greenery_equipment/settings.py
+++ greenery_equipment/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-djkfi6^aj^b2+q(7-p1@oi^v8&6f#5ona9vl09-bvqxqxku_2x'
+SECRET_KEY = os.environ.get('SECRET_KEY' '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['greenery-equipment.herokuapp.com', 'localhost']
 
